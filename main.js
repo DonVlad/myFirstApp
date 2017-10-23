3d4727df7a1d42d33e75d60d8015946c451cb51f
Vlad <boykovdon@gmail.com>
2017-10-23 23:07:06 +0300

    something to do

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //main fail of my app
 var number = process.argv[2];
 var message = process.argv[3];
+
 var PrintMessage = require("./design");
+var CountOfSymbles = require("./another");
 
 PrintMessage(number,message);
+
+console.log(message.length*number);
